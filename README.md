6f8536956d6441cf58686aa11716831029d7ea84
Joon Ruiz <jruiz@Joons-MacBook-Pro.local>
2022-12-05 20:52:15 -0800

    Update on readme.md

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Module6Challenge
 
 Module 6 Challenge for UW bootcamp
+Weather News
+
+## Table of Content
+* General Information
+* Technologies Used
+* Set Up & Installation
+* Use
+
+## General Information
+
+This Web Application is to learn and know weather of the location that user is seeking.
+Once type and entering the location of the city ex. Seattle, Los Angeles.... etc. 
+It will display the day of the weather and next 5 day of the weather in the screen. 
+It has functionality to store search history and to clear the search history.
+
+## Technologies Used
+
+HTML
+CSS
+JavaScript
+Jquery 3.6.1 [https://releases.jquery.com/]
+
+## Set Up and Installation
+
+This application does not require installation. It is published on Github Page as follows:
+
+
+
+
+## Use
+
+This application is free to use. 
--- assets/script.js
+++ assets/script.js
@@ -1,152 +1,197 @@
 // Function will run once html is fully loaded.
 $(function () {
-        // Global variables
-    let cityInput = $('#city-input')
-    let btnEle = $('#btn')
-    let fiveWeather = $('#fiveWeather')
-    let weatherDiv = $('#weather-display')
-    let userParam = cityInput.val()
-    let fiveWeatherIcon = $('#fiveWeatherIcon')
-    let currentWeather = $('#current-weather')
+  // Global variables
+  let cityInput = $("#city-input");
+  let btnEle = $("#btn");
+  let fiveWeather = $("#fiveWeather");
+  let weatherDiv = $("#weather-display");
+  let userParam = cityInput.val();
+  let currentWeather = $("#current-weather");
+  let currTemp = $("#curr-temp");
+  let currWeather = $("#curr-weather");
+  let currHumid = $("#curr-humidity");
+  let container = $("#container");
+  let searchHistory = $("#search-history");
+  let clearSearch = $("#clearHistory");
 
-        // IMG address
-    let cloudsIcon = 'https://img.icons8.com/dotty/512/clouds.png'
-    let rainIcon = 'https://img.icons8.com/dotty/512/heavy-rain.png'
-    let sunnyIcon = 'https://img.icons8.com/dotty/512/sun.png'
-    let thunderIcon = 'https://img.icons8.com/dotty/512/storm.png'
-    let snowIcon  = 'https://img.icons8.com/dotty/512/snow.png'
+  let retriveJson = JSON.parse(localStorage.getItem("dkey")) || [];
 
-        //API PARAMS
-    let requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='
-    let param1 = '&limit=5&appid=a763be4befc9d6fd3981fb3420b2dcb0'
-    let completeApi = requestUrl + userParam + param1
-    let weatherurl = 'http://api.openweathermap.org/data/2.5/forecast?lat='
-    let weatherurl2 = '&lon='
-    let weatherurl3 = '&units=imperial&appid=a763be4befc9d6fd3981fb3420b2dcb0'
+  // IMG address
+  let cloudsIcon = "https://img.icons8.com/dotty/512/clouds.png";
+  let rainIcon = "https://img.icons8.com/dotty/512/heavy-rain.png";
+  let sunnyIcon = "https://img.icons8.com/dotty/512/sun.png";
+  let thunderIcon = "https://img.icons8.com/dotty/512/storm.png";
+  let snowIcon = "https://img.icons8.com/dotty/512/snow.png";
 
-    // Fetching API to get data by location name
-    function getApi(completeApi) {
-        fetch(completeApi, {
-            method: 'GET',
+  //API PARAMS
+  let requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
+  let param1 = "&limit=5&appid=a763be4befc9d6fd3981fb3420b2dcb0";
+  let completeApi = requestUrl + userParam + param1;
+  let weatherurl = "http://api.openweathermap.org/data/2.5/forecast?lat=";
+  let weatherurl2 = "&lon=";
+  let weatherurl3 = "&units=imperial&appid=a763be4befc9d6fd3981fb3420b2dcb0";
+
+  // Fetching API to get data by location name
+  function getApi(completeApi) {
+    // This will empty inside element when api is called to prevent duplicates display
+    fiveWeather.empty();
+    currTemp.empty();
+    currWeather.empty();
+    currHumid.empty();
+    container.empty();
+    searchHistory.empty();
+
+    fetch(completeApi, {
+      method: "GET",
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // Declaring var to use it for nested API to grab weather information by adding lat and lon from the first API
+        let lat = data[0].lat;
+        let latString = lat.toString();
+        let lon = data[0].lon;
+        let longString = lon.toString();
+        let completeApi2 =
+          weatherurl + latString + weatherurl2 + longString + weatherurl3;
+
+        // Nested API using data from first fetch above to grab lan and lon to grab weather information
+        fetch(completeApi2, {
+          method: "GET",
         })
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (info) {
+            //assigning var to information that is needed for appending to html
+            let weather = info.list[0].weather[0].main;
+            let temperture = info.list[0].main.temp;
+            let humidity = info.list[0].main.humidity;
 
-                // Declaring var to use it for nested API to grab weather information by adding lat and lon from the first API
-                let lat = data[0].lat;
-                let latString = lat.toString();
-                let lon = data[0].lon;
-                let longString = lon.toString();
-                let completeApi2 = weatherurl + latString + weatherurl2 + longString + weatherurl3;
+            // appending variables to the weather display
+            currWeather.append("Today Weather Is: " + weather);
+            currTemp.append("Current Temperture Is: " + temperture + " °F");
+            currHumid.append("Humidity Today is: " + humidity + "%");
 
-                // Nested API using data from first fetch above to grab lan and lon to grab weather information
-                fetch(completeApi2, {
-                    method: 'GET',
-                })
-                    .then (function (response){
-                        return response.json();
-                    })
-                    .then(function (info){
+            // If condition to check the weather data and append img src of correct img to display on html
+            if (weather === "Clouds") {
+              $(document).ready(function () {
+                $(
+                  `<img src='${cloudsIcon}' style="height: 50px; width: 50px;">`
+                ).appendTo(container);
+              });
+            } else if (weather === "Rain") {
+              $(document).ready(function () {
+                $(
+                  `<img src='${rainIcon}'style="height: 50px; width: 50px; ">`
+                ).appendTo(container);
+              });
+            } else if (weather === "Clear") {
+              $(document).ready(function () {
+                $(
+                  `<img src='${sunnyIcon}' style="height: 50px; width: 50px;">`
+                ).appendTo(container);
+              });
+            } else if (weather === "Thunderstorm") {
+              $(document).ready(function () {
+                $(
+                  `<img src='${thunderIcon}' style="height: 50px; width: 50px;">`
+                ).appendTo(container);
+              });
+            } else {
+              $(document).ready(function () {
+                $(
+                  `<img src='${snowIcon}' style="height: 50px; width: 50px;">`
+                ).appendTo(container);
+              });
+            }
 
-                        //assigning var to information that is needed for appending to html
-                        console.log(info)
-                        let weather = info.list[0].weather[0].main
-                        let temperture = info.list[0].main.temp
-                        let humidity = info.list[0].main.humidity
+            // Display 5 days weather forecast of the city using For loops to grab data by var i
+            for (var i = 0; i < 5; i++) {
+              let fiveDayWeather = info.list[i + 1].weather[0].main;
+              let fiveDayTemp = info.list[i + 1].main.temp;
+              let fiveDayHumid = info.list[i + 1].main.humidity;
+              let fiveWeatherDiv = document.createElement("div");
+              let fiveWeatherUlEle = document.createElement("ul");
+              let fiveWeatherLiEle = document.createElement("li");
+              fiveWeatherLiEle.append("Weather: " + fiveDayWeather + "   | ");
+              fiveWeatherLiEle.append("Temperture: " + fiveDayTemp + "°F  | ");
+              fiveWeatherLiEle.append("Humidity: " + fiveDayHumid + "   %");
+              fiveWeatherUlEle.append(fiveWeatherLiEle);
+              fiveWeatherDiv.append(fiveWeatherUlEle);
+              fiveWeather.append(fiveWeatherDiv);
 
-                        // appending variables to the weather display
-                        let currWeather = $('#curr-weather').append('Today Weather Is: ' + weather)
-                        let currTemp = $('#curr-temp').append('Current Temperture Is: ' + temperture + ' °F')
-                        let currHumid = $('#curr-humidity').append('Humidity Today is: ' + humidity + '%')
+              // Display image of each day of the weather
+              if (fiveDayWeather === "Clouds") {
+                $(document).ready(function () {
+                  $(
+                    `<img src='${cloudsIcon}' style="height: 50px; width: 50px;">`
+                  ).appendTo(fiveWeatherDiv);
+                });
+              } else if (fiveDayWeather === "Rain") {
+                $(document).ready(function () {
+                  $(
+                    `<img src='${rainIcon}'style="height: 50px; width: 50px; ">`
+                  ).appendTo(fiveWeatherDiv);
+                });
+              } else if (fiveDayWeather === "Clear") {
+                $(document).ready(function () {
+                  $(
+                    `<img src='${sunnyIcon}' style="height: 50px; width: 50px;">`
+                  ).appendTo(fiveWeatherDiv);
+                });
+              } else if (fiveDayWeather === "Thunderstorm") {
+                $(document).ready(function () {
+                  $(
+                    `<img src='${thunderIcon}' style="height: 50px; width: 50px;">`
+                  ).appendTo(fiveWeatherDiv);
+                });
+              } else {
+                $(document).ready(function () {
+                  $(
+                    `<img src='${snowIcon}' style="height: 50px; width: 50px;">`
+                  ).appendTo(fiveWeatherDiv);
+                });
+              }
+            }
+          });
+      });
+  }
+  // Trigger API call and display class="showP".
+  btnEle.on("click", function (e) {
+    e.preventDefault();
+    getApi(completeApi);
+    $(".showP").css("display", "block");
+    currentWeather.attr("id", "current-weatherB");
 
-                        // If condition to check the weather data and append img src of correct img to display on html
-                        if (weather === 'Clouds'){
-                            $(document).ready(function() {
-                                $(`<img src='${cloudsIcon}' style="height: 50px; width: 50px;">`).appendTo('#container');
-                            });
-                        } else if (weather === 'Rain'){
-                            $(document).ready(function() {
-                                $(`<img src='${rainIcon}'style="height: 50px; width: 50px; >`).appendTo('#container');
-                            });
-                        } else if (weather === 'Clear'){
-                            $(document).ready(function() {
-                                $(`<img src='${sunnyIcon}' style="height: 50px; width: 50px;>`).appendTo('#container');
-                            });
-                        } else if (weather === 'Thunderstorm'){
-                            $(document).ready(function() {
-                                $(`<img src='${thunderIcon}' style="height: 50px; width: 50px;>`).appendTo('#container');
-                            });
-                        } else {
-                            $(document).ready(function() {
-                                $(`<img src='${snowIcon}' style="height: 50px; width: 50px;>`).appendTo('#container');
-                            });
-                        }
+    // Get and SetItem to localstorage and allowing value of the object to be displayed in the
+    // Search history-container
+    let dKey = "city";
+    let dVal = $(this).siblings("#city-input").val();
+    let keyItem = { [dKey]: dVal };
+    retriveJson.push(keyItem);
+    localStorage.setItem("dkey", JSON.stringify(retriveJson)) || [];
+    let getLocalItem = JSON.parse(localStorage.getItem("dkey")) || [];
 
-
-                        // Display 5 days weather forecast of the city using For loops to grab data by var i
-                        for (var i = 0; i < 5; i ++){
-
-                            let fiveDayWeather = info.list[i + 1].weather[0].main
-                            let fiveDayTemp = info.list[i + 1].main.temp
-                            let fiveDayHumid = info.list[i + 1].main.humidity
-                            let fiveWeatherDiv = document.createElement('div')
-                            let fiveWeatherUlEle = document.createElement('ul')
-                            let fiveWeatherLiEle = document.createElement('li')
-                            fiveWeatherLiEle.append('Weather: ' + fiveDayWeather + '   | ')
-                            fiveWeatherLiEle.append('Temperture: ' + fiveDayTemp + '°F  | ')
-                            fiveWeatherLiEle.append('Humidity: ' + fiveDayHumid + '   %')
-                            fiveWeatherUlEle.append(fiveWeatherLiEle)
-                            fiveWeatherDiv.append(fiveWeatherUlEle)
-                            fiveWeather.append(fiveWeatherDiv)
-
-                            if (fiveDayWeather === 'Clouds'){
-                                $(document).ready(function() {
-                                    $(`<img src='${cloudsIcon}' style="height: 50px; width: 50px;">`).appendTo('#fiveWeatherDiv');
-                                });
-                            } else if (fiveDayWeather === 'Rain'){
-                                $(document).ready(function() {
-                                    $(`<img src='${rainIcon}'style="height: 50px; width: 50px; >`).appendTo('#fiveWeatherDiv');
-                                });
-                            } else if (fiveDayWeather === 'Clear'){
-                                $(document).ready(function() {
-                                    $(`<img src='${sunnyIcon}' style="height: 50px; width: 50px;>`).appendTo('#fiveWeatherDiv');
-                                });
-                            } else if (fiveDayWeather === 'Thunderstorm'){
-                                $(document).ready(function() {
-                                    $(`<img src='${thunderIcon}' style="height: 50px; width: 50px;>`).appendTo('#fiveWeatherDiv');
-                                });
-                            } else {
-                                $(document).ready(function() {
-                                    $(`<img src='${snowIcon}' style="height: 50px; width: 50px;>`).appendTo('#fiveWeatherDiv');
-                                });
-
-                            }
-                        }
-                    })
-            })
+    for (var i = 0; i < getLocalItem.length; i++) {
+      let creatBnt = document.createElement("li");
+      creatBnt.append(getLocalItem[i].city);
+      searchHistory.append(creatBnt);
     }
+  });
+  // Detect input from user
+  cityInput.on("keyup", function () {
+    userParam = cityInput.val();
+    completeApi = requestUrl + userParam + param1;
+  });
 
-    btnEle.on('click', function(e){
-        e.preventDefault();
-        getApi(completeApi)
-        $('.showP').css('display', 'block')
-        currentWeather.attr('id', 'current-weatherB')
-    })
+  clearSearch.on("click", function () {
+    localStorage.clear();
+    searchHistory.empty();
+    localStorage.removeItem("city");
+    window.location.reload();
+  });
 
-    cityInput.on('keyup', function(){
-        userParam = cityInput.val();
-        completeApi = requestUrl + userParam + param1
-    })
 
-    // let numbers = [1,2,3,4,5,6]
-    // for (let i in numbers){
-    //     let key = i;
-    //     let value = userParam
-    //     let test = {[key]: value};
-    //     console.log(test)
-}
-
-)
+});
